cd823b2ecf6c16ef115e755869c93e4d5a4e8018
jerimsutton-cmyk <jerimsutton@gmail.com>
2026-09-10 15:13:52 -0700

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path');
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname));
+app.use(express.static(__dirname));
 
 // Salesforce connection
 let sfConn = null;
